fcb82eea08cb4c4469b74900e154fe7de6f5b0f6
vasilevdimitirj <92320851+vasilevdimitirj@users.noreply.github.com>
2021-10-25 21:40:48 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+
+d = new Date()
+document.writeln("Local date " + d.toLocaleString() + "<br>")
+document.writeln("Today is " + d.getDate() + "/" + (d.getMonth() + 1) + "/" + d.getFullYear() + "<br>")
